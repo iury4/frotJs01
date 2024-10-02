ce59a0ad5f602fbaab1b7e0c2d02371b06fe09a9
pablo marçal <iuryryaan30@gmail.com>
2024-10-02 12:26:32 +0000

    formulario limpar

--- src/index.mjs
+++ src/index.mjs
@@ -2,24 +2,42 @@
 const app = document.querySelector("#app");
 const numero1 = document.querySelector("#numero1");
 const numero2 = document.querySelector("#numero2");
-const btnCalcular = document.querySelector("btn-calcular");
+const btnCalcular = document.querySelector("#btn-calcular");
+const btnLimpar = document.querySelector("#btn-limpar");
 
 const resultados = document.querySelector("#resultados");
 const soma = document.querySelector(".soma");
-const multiplicacao = document.querySelector(".multiplicação");
-const subtracao = document.querySelector(".subtração");
-const divisao = document.querySelector(".divisão");
+const multiplicacao = document.querySelector(".multiplicacao");
+const subtracao = document.querySelector(".subtracao");
+const divisao = document.querySelector(".divisao");
 
 function calcular() {
   const n1 = Number(numero1.value);
   const n2 = Number(numero2.value);
+  
+
   soma.innerHTML = `A soma de ${n1} e ${n2} é: ${n1 + n2} `;
-  //return res;
+
+  subtracao.innerHTML = `A subtracação de ${n1} e ${n2} é: ${n1 - n2} `;
+
+  multiplicacao.innerHTML = `A multiplicação de ${n1} e ${n2} é: ${n1 * n2} `;
+
+  divisao.innerHTML = `A DIVISÃO de ${n1} e ${n2} é: ${n1 / n2} `;
 }
-
-calcular();
-
-btnCalcular.addEventLidtener("click", function (evento) {
-  evento.preventDefaul();
+btnCalcular.addEventListener("click", function (evento) {
+  evento.preventDefault();
   calcular();
 });
+btnLimpar.addEventListener("click", function (evento) {
+  evento.preventDefault();
+  limparFormulario();
+});
+
+
+function limparFormulario() {
+  const n1 = Number(numero1.value);
+  const n2 = Number(numero2.value);
+  if (n1 || n2) {
+    const form = document.querySelector("form");
+    form.reset();
+    document.querySelector("#numero1").focus();
